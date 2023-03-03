f7f529534948a97852e8d1314b8192c18df00074
Victor Melo <vbmelo@ua.pt>
2023-03-03 01:04:25 +0000

    Programa feito, parser e request handler a funcionar

---
+++ lab1/lab1_1stack/Geocoding/src/main/java/org/example/TqsBasicHttpClient.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import java.io.IOException;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+public class TqsBasicHttpClient implements ISimpleHttpClient {
+    @Override
+    public String doHttpGet(String url) throws IOException {
+
+        CloseableHttpClient client = HttpClients.createDefault();
+        HttpGet request = new HttpGet(url);
+        CloseableHttpResponse response = client.execute(request);
+
+        try {
+            HttpEntity entity = response.getEntity();
+            return EntityUtils.toString(entity);
+        } finally {
+            if(response != null)
+                response.close();
+        }
+    }
+}
