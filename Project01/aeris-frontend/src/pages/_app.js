166a5b815889efe011614fd9a001a5fe7ee28125
Victor Melo <vbmelo@ua.pt>
2023-04-02 18:05:38 +0100

    NextJs working with Bootstrap and Phosphor-Icons

--- Project01/aeris-frontend/src/pages/_app.js
+++ Project01/aeris-frontend/src/pages/_app.js
@@ -1,5 +1,15 @@
-import '@/styles/globals.css'
+import Head from "next/head";
+import "@/styles/globals.css";
+// add bootstrap css
+import "bootstrap/dist/css/bootstrap.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return (
+		<>
+			<Head>
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+			</Head>
+			<Component {...pageProps} />
+		</>
+	);
 }
